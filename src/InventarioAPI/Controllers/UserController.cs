1d5e91fb5f7ecbb06aa5d342302695b8d7f2b9a0
AlexisUFG <ia.alexismartinez@ufg.edu.sv>
2024-11-13 01:01:25 -0600

    :sparkles: add user Controller CRUD

---
+++ src/InventarioAPI/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using InventarioDAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(Ciclo8InventarioContext context) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get(int? page = null, int? pageSize = null)
+        {
+            if (page <= 0)
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Type = "https://tools.ietf.org/html/rfc9110#section-15.5.5",
+                    Title = "Bad Request",
+                    Detail = "page cannot be equal to or less than 0"
+                });
+            }
+
+            var query = context.Users.AsQueryable();
+
+            if (page.HasValue || pageSize.HasValue)
+            {
+                int currentPage = page ?? 1;
+                int recordsPerPage = pageSize.HasValue ? Math.Min(pageSize.Value, 20) : 20;
+
+                query = query
+                    .Skip((currentPage - 1) * recordsPerPage)
+                    .Take(recordsPerPage);
+            }
+
+            return Ok(await query.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.IdUser == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Put([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.IdUser == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
