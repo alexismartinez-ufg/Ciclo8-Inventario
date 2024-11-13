d6aec8f61e7b207ffd79ee36cb11126c79c1a3b5
AlexisUFG <ia.alexismartinez@ufg.edu.sv>
2024-11-12 23:45:07 -0600

    :sparkles: add client controller CRUD

---
+++ src/InventarioAPI/Controllers/ClientController.cs
@@ -0,0 +1,94 @@
+﻿using InventarioDAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController(Ciclo8InventarioContext context) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Client>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get(int? page = null, int? pageSize = null)
+        {
+            var query = context.Clients.AsQueryable();
+
+            if (page.HasValue || pageSize.HasValue)
+            {
+                int currentPage = page ?? 1;
+                int recordsPerPage = pageSize.HasValue ? Math.Min(pageSize.Value, 20) : 20;
+
+                query = query
+                    .Skip((currentPage - 1) * recordsPerPage)
+                    .Take(recordsPerPage);
+            }
+
+            return Ok(await query.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var department = await context.Clients.FirstOrDefaultAsync(x => x.IdClient == id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Post([FromBody] Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await context.Clients.AddAsync(client);
+            await context.SaveChangesAsync();
+
+            return Ok(client);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Put([FromBody] Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            context.Clients.Update(client);
+            await context.SaveChangesAsync();
+
+            return Ok(client);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var department = await context.Clients.FirstOrDefaultAsync(x => x.IdClient == id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            context.Clients.Remove(department);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
